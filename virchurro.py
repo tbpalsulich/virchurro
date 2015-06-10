938e3da94c10c0e6cadada0ded00acb6a02373b4
Tyler Palsulich <tpalsulich@gmail.com>
2015-06-10 03:48:31 -0700

    Use secrets file.

--- virchurro.py
+++ virchurro.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, url_for
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+from werkzeug.exceptions import abort
+import secrets
 
 app = Flask(__name__)
+app.config.from_object(secrets)
 
 @app.route("/")
 def index():
