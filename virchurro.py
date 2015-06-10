d60570c72634914e59f38b578f668cd241422a18
Tyler Palsulich <tpalsulich@gmail.com>
2015-06-10 03:49:01 -0700

    Initial creation of new virchurros!

--- virchurro.py
+++ virchurro.py
@@ -11,9 +11,32 @@ app.config.from_object(secrets)
 def index():
     return render_template("index.html")
 
+@app.route("/new", methods=["POST"])
+def new():
+    c = g.db.cursor()
+    c.execute('insert into entries (song) values (?)', [request.form['song']])
+    g.db.commit()
+    return redirect(url_for("get", id=c.lastrowid))
+
 @app.route("/<id>")
 def get(id):
-    return render_template("index.html", id=id)
+    song = g.db.execute('SELECT song FROM entries WHERE id=?', [id]).fetchone()
+    if song is None:
+        abort(404)
+    return render_template("churro.html", song=song[0])
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
 
 if __name__ == "__main__":
     app.run(debug=True)
